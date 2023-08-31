3692f8ec95d356c3b4f516ff2bb9b0c499747008
Ivo Spassov <95618674+ivospassov@users.noreply.github.com>
2023-08-31 09:27:48 +0300

    Update GetVillainsNames.java

--- GetVillainsNames.java
+++ GetVillainsNames.java
@@ -9,7 +9,7 @@ public class GetVillainsNames {
         //GET VILLAINS' NAMES BASED ON NUMBER OF MINIONS
         Properties properties = new Properties();
         properties.setProperty("user", "root");
-        properties.setProperty("password", "am99e3n8");
+        properties.setProperty("password", "pass");
 
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/minions_db", properties);
 
